cd121d5d2291a75860a9edd86d0d50c8b552da25
boris-shakhverdyan <boris.shakhverdyan@gmail.com>
2024-05-14 13:04:32 +0400

    add query cache for Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,7 @@ use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Staudenmeir\EloquentJsonRelations\HasJsonRelationships;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 /**
  * @property  int     $id
@@ -25,7 +26,38 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class Product extends Model
 {
-    use HasFactory, HasJsonRelationships, LogsActivity;
+    use HasFactory, HasJsonRelationships, LogsActivity, QueryCacheable;
+
+    /**
+     * Specify the amount of time to cache queries.
+     * Do not specify or set it to null to disable caching.
+     *
+     * @var int|\DateTime
+     */
+    public $cacheFor = 3600;
+
+    /**
+     * The tags for the query cache. Can be useful
+     * if flushing cache for specific tags only.
+     *
+     * @var null|array
+     */
+    public $cacheTags = ['products'];
+
+    /**
+     * A cache prefix string that will be prefixed
+     * on each cache key generation.
+     *
+     * @var string
+     */
+    public $cachePrefix = 'products_';
+
+    /**
+     * The cache driver to be used.
+     *
+     * @var string
+     */
+    public $cacheDriver = 'file';
 
     protected $fillable = [
         "title",
